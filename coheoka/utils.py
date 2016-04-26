fa5f9ad96c52d5977913a4e8c41c0ec0bd2214f1
Lumit <to.be.impressive@gmail.com>
2016-04-26 17:01:52 +0800

    Add util function to score sentences with kendall's tau

--- coheoka/utils.py
+++ coheoka/utils.py
@@ -2,8 +2,11 @@
 '''
 Preprocessing utilities
 '''
-from nltk import sent_tokenize
+
 from random import shuffle, sample
+
+from nltk import sent_tokenize
+from scipy.stats import kendalltau as tau
 
 
 def shuffle_sents(text, times):
@@ -31,3 +34,14 @@ def add_sents(text, times, added_text, add_number=1):
         shuffle(sents)
         res.append(' '.join(sents))
     return res
+
+
+def tau_score_of_sentents(sent1_tokens, sent2_tokens):
+    assert len(sent1_tokens) == len(sent2_tokens)
+    t = tau(sent1_tokens, sent2_tokens)[0]
+    if t <= 0.33:
+        return -1
+    elif t > 0.33 and t <= 0.66:
+        return 0
+    else:
+        return 1
